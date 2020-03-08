4387e6536b83f344c1e7632fc31e5e449328b67e
iwpnd <legionaerr@googlemail.com>
2020-03-08 10:57:18 +0100

    removed initial test_producer method that relied on a running kafka instance

--- geostream/producer/tests/test_producer.py
+++ geostream/producer/tests/test_producer.py
@@ -3,18 +3,6 @@ from collections import namedtuple
 from aiokafka import AIOKafkaProducer
 
 
-def test_post_producer(test_app):
-
-    msg = {"name": "AIOmessenger_ONE", "lat": 13, "lon": 37}
-
-    topicname = "geostream"
-
-    with test_app as client:
-        response = client.post(f"/producer/{topicname}", json=msg)
-
-    assert response.status_code == 200
-
-
 def test_post_producer_monkey(test_app, monkeypatch):
 
     msg = {"name": "AIOmessenger_ONE", "lat": 13, "lon": 37}
