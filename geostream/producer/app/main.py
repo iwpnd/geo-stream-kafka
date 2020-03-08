c944d880a86e9786f6b47fda734fc323de6105c2
iwpnd <legionaerr@googlemail.com>
2020-03-08 11:05:13 +0100

    added docstring and debug to start/stop

--- geostream/producer/app/main.py
+++ geostream/producer/app/main.py
@@ -7,26 +7,38 @@ from app.core.models.model import ProducerResponse
 from fastapi import FastAPI
 from loguru import logger
 
-app = FastAPI()
+app = FastAPI(title="geostream_kafka_producer")
+
 
 loop = asyncio.get_event_loop()
 aioproducer = AIOKafkaProducer(
-    loop=loop, client_id="geostream-kafka", bootstrap_servers="kafka:9092"
+    loop=loop, client_id="geostream-kafka-producer", bootstrap_servers="kafka:9092"
 )
 
 
 @app.on_event("startup")
 async def startup_event():
+    logger.debug("starting aiokafka producer")
     await aioproducer.start()
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
+    logger.debug("stopping aiokafka producer")
     await aioproducer.stop()
 
 
 @app.post("/producer/{topicname}")
 async def kafka_produce(msg: ProducerMessage, topicname: str):
+    """
+    Produce a message into <topicname>
+
+    This will produce a message into a Apache Kafka topic
+
+    And this path operation will:
+
+    * return ProducerResponse
+    """
 
     result = await aioproducer.send_and_wait(
         topicname, json.dumps(msg.dict()).encode("ascii")
