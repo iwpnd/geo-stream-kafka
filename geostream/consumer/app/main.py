b7950e5f9ed6601f033cf81fc5e0bd5062500aca
iwpnd <legionaerr@googlemail.com>
2020-03-09 09:27:27 +0100

    websockets

--- geostream/consumer/app/main.py
+++ geostream/consumer/app/main.py
@@ -3,11 +3,13 @@ import asyncio
 from aiokafka import AIOKafkaConsumer
 from app.core.config import KAFKA_INSTANCE
 from app.core.config import PROJECT_NAME
-from app.core.models.model import ConsumerResponse
 from fastapi import FastAPI
-from loguru import logger
+from fastapi import WebSocket
+from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
 
+# from app.core.models.model import ConsumerResponse
+# from loguru import logger
 
 app = FastAPI(title=PROJECT_NAME)
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
@@ -37,23 +39,71 @@ async def consume(topicname):
         await consumer.stop()
 
 
-@app.get("/consumer/{topicname}")
-async def kafka_consume(topicname: str):
-    """
-    Consume messages from <topicname>
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://127.0.0.1:8003/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-    This will start a Kafka Consumer from a topic
 
-    And this path operation will:
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
 
-    * return ConsumerResponse
-    """
-    cr = ConsumerResponse()
-    print(cr)
-    msg = await consume(topicname)
-    logger.info(msg.decode())
 
-    return msg
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await consume("geostream")
+        await websocket.send_text(f"Message text was: {data}")
+
+
+# @app.get("/consumer/{topicname}")
+# async def kafka_consume(topicname: str):
+#     """
+#     Consume messages from <topicname>
+
+#     This will start a Kafka Consumer from a topic
+
+#     And this path operation will:
+
+#     * return ConsumerResponse
+#     """
+#     cr = ConsumerResponse()
+#     print(cr)
+#     msg = await consume(topicname)
+#     logger.info(msg.decode())
+
+#     return msg
 
 
 @app.get("/ping")
