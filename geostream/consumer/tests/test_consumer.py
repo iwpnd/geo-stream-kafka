3650d82d8f469eccf25ebf05601aa626134da995
iwpnd <legionaerr@googlemail.com>
2020-03-09 15:59:06 +0100

    working websocketendpoint including passing async tests

--- geostream/consumer/tests/test_consumer.py
+++ geostream/consumer/tests/test_consumer.py
@@ -1,10 +1,16 @@
+import asyncio
+
+import pytest
+
+
 def test_get_consumer(test_app, monkeypatch):
     pass
 
 
-def test_consumer_websocket(test_app):
+@pytest.mark.asyncio
+async def test_consumer_websocket(test_app):
     with test_app.websocket_connect("/consumer/geostream") as websocket:
         data = websocket.receive_json()
-    websocket.close()
+        await asyncio.sleep(0.1)
 
-    assert data == {"connected_to": "geostream"}
+    assert data == {"Message: ": "connected"}
