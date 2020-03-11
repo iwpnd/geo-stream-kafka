b14d6b5b62cf6726fe5b4472959a1a3a951b53c3
iwpnd <legionaerr@googlemail.com>
2020-03-11 10:15:23 +0100

    update frontend to draw polylines on from events and group them by event.data.name

--- geostream/frontend/app/static/map.js
+++ geostream/frontend/app/static/map.js
@@ -4,29 +4,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-mapMarkers1 = [];
-mapMarkers2 = [];
+mapmarkers = {};
 
 var ws = new WebSocket("ws://127.0.0.1:8003/consumer/geostream");
 ws.onmessage = function(event) {
     console.log(event.data)
-
     obj = JSON.parse(event.data)
 
-    if(obj.name == "AIOmessenger_ONE") {
-      for (var i = 0; i < mapMarkers1.length; i++) {
-        map.removeLayer(mapMarkers1[i]);
-      }
-      marker1 = L.circle([obj.lat, obj.lon], {color: 'red', radius: 100}).addTo(map);
-      mapMarkers1.push(marker1);
+    if(!(obj.name in mapmarkers)) {
+      mapmarkers[obj.name] = [];
+      console.log(mapmarkers)
+      mapmarkers[obj.name].push([obj.lat, obj.lon]);
+    }
+    else {
+      console.log(mapmarkers)
+      mapmarkers[obj.name].push([obj.lat, obj.lon]);
     }
 
-    if(obj.name == "AIOmessenger_TWO") {
-      for (var i = 0; i < mapMarkers2.length; i++) {
-        map.removeLayer(mapMarkers2[i]);
-      }
-      marker2 = L.circle([obj.lat, obj.lon], {color: 'blue', radius: 100}).addTo(map);
-      mapMarkers2.push(marker2);
-    }
+    marker = L.polyline(mapmarkers[obj.name], {color: 'blue', radius: 100}).addTo(map);
 
 };
